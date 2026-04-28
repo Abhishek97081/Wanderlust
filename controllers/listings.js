3aeec921e3833379c05f622af8f9ce6e9ae5658d
Abhishek97081 <abhisinghrajput970@gmail.com>
2026-04-28 11:33:27 +0530

    map is added using mapbox

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,4 +1,7 @@
 const Listing = require("../Models/listing.js");
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mapToken=process.env.Map_Token;
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 module.exports.index=async (req, res) => {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", { allListings });
@@ -23,7 +26,17 @@ module.exports.index=async (req, res) => {
       res.render("listings/show.ejs", { listing });
     };
     module.exports.createListing=async (req, res) => {
-      let url=req.file.path;
+
+   let response= await  geocodingClient.forwardGeocode({
+  query: req.body.listing.location,
+  limit: 1,
+})
+  .send();
+  
+
+
+
+let url=req.file.path;
       let filename=req.file.filename;
        console.log(url, " ",filename);
         const newListing = new Listing(req.body.listing);
@@ -39,7 +52,8 @@ module.exports.index=async (req, res) => {
         }
     newListing.image.url=url;
     newListing.image.filename=filename;
-        await newListing.save();
+    newListing.geometry=response.body.features[0].geometry;
+     let savedListing = await newListing.save();
     
         req.flash("success", "Successfully created a new listing!");
         res.redirect("/listings");
