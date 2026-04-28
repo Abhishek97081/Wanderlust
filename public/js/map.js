b768cea91e674cda8d04c3cf550f8ecdbe32f628
Abhishek97081 <abhisinghrajput970@gmail.com>
2026-04-28 11:52:34 +0530

    marker and popup added using mapbox api

--- public/js/map.js
+++ public/js/map.js
@@ -3,6 +3,18 @@
     mapboxgl.accessToken = mapToken;
     const map = new mapboxgl.Map({
         container: 'map', // container ID
-        center: [-71.06776, 42.35816], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+        center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
         zoom: 9 // starting zoom
     });
+
+console.log(coordinates);
+
+
+ const marker1 = new mapboxgl.Marker( { color: 'red' })
+        .setLngLat(listing.geometry.coordinates)
+        .setPopup(new mapboxgl.Popup({offset: 25,})
+    .setLngLat(e.lngLat)
+    .setHTML("<h4>" + listing.location + "</h4><p>Exact location provided after booking</p>")
+    .setMaxWidth("300px"))
+    .addTo(map);
+        
